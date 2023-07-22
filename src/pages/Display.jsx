5dc8af5a3b8ac793cc545af2627a313ff1c064b9
Srusti Prusty <srustiprusty@gmail.com>
2023-07-23 01:35:57 +0530

    display and upload

--- src/pages/Display.jsx
+++ src/pages/Display.jsx
@@ -1,5 +1,20 @@
 import "../styles/Display.css";
+import { Typewriter } from 'react-simple-typewriter'
 
 export default function Display() {
-  return <div>Display</div>;
+  return <div className="whole">
+  <h1 className="heading-opt"> Here's what you can do ♻️</h1>
+      <div className="holder" id="txt-holder"> 
+      <Typewriter
+            words={["We've analyzed the product you uploaded and confirmed that it is made of plastic. Plastic recycling plays a crucial role in reducing waste and conserving valuable resources, so we're excited to guide you through the various ways you can recycle it responsibly. 1) CURBSIDE RECYCLING: Check with your local recycling program to see if they accept this type of plastic. Many municipalities have curbside recycling services that make it easy to dispose of recyclable plastics at home. 2) DROP-OFF RECYCLING CENTRES: Look for recycling centers or collection points in your area that specifically handle plastic items. These centers are dedicated to recycling various types of plastics and ensure they are processed correctly. 3) ECOBRICK INITIATIVES: If the plastic product is not recyclable through traditional means, consider participating in eco-brick initiatives. Eco-bricks are made by stuffing non-recyclable plastics into plastic bottles, creating building blocks for sustainable construction projects. 4) MAIL-IN RECYCLING PROGRAMS: Some companies and organizations offer mail-in recycling programs for specific plastic products. You can send the item to them for proper recycling. 5) DONATION OR REPURPOSING: If your plastic product is still in good condition, consider donating it to local organizations or charities that can find alternative uses for it.We've analyzed the product you uploaded and confirmed that it is made of plastic. Plastic recycling plays a crucial role in reducing waste and conserving valuable resources, so we're excited to guide you through the various ways you can recycle it responsibly."]}
+            
+            cursor
+            cursorStyle='|'
+            typeSpeed={10}
+            delaySpeed={10}
+          />
+     
+</div>
+
+</div>
 }
--- src/pages/Upload.jsx
+++ src/pages/Upload.jsx
@@ -1,5 +1,30 @@
 import "../styles/Upload.css";
 
 export default function Upload() {
-  return <div>Upload</div>;
+  return (
+    <div className="body">
+      <div className="container1 d-flex justify-content-center flex-column align-items-center">
+        <input type="file" id="file" accept="image/*" hidden />
+        <div className="img-area1" data-img="">
+          <i className="bx bxs-cloud-upload icon"></i>
+          <h3>Upload Image</h3>
+          <p>
+            Image size must be less than <span>2MB</span>
+          </p>
+        </div>
+        <input type="file" name="" className="select-image " id="" />
+        <button className= "select-image" style={{marginTop:'1rem'}}>Submit</button>
+      </div>
+
+      <div className="content-part">
+        <h1>
+          {" "}
+          Congratulations! <br />{" "}
+        </h1>
+        <h2> The product you've uploaded is recyclable. </h2>
+        <h3> Thank you for making an environmental choice. </h3>
+        <button className="see-options">See recycling options</button>
+      </div>
+    </div>
+  );
 }
