e56c2cde3b3b30dce038968958b3c8bfb85ada7e
Anil Kumar Behera <96728156+Anonymous961@users.noreply.github.com>
2023-07-23 12:45:47 +0530

    every error fixed

--- src/hooks/useFetch.jsx
+++ src/hooks/useFetch.jsx
@@ -1,28 +1,28 @@
-import axios from "axios";
-import { useState } from "react";
+// import axios from "axios";
+// import { useState } from "react";
 
-const useFetch = () => {
-  const [data, setData] = useState();
-  const [loading, setLoading] = useState();
-  const [error, setError] = useState();
-  console.log(import.meta.env.VITE_APP_BASE_URL);
-  try {
-    const fetchData = async () => {
-      setLoading(true);
-      const data = await axios.post(import.meta.env.VITE_APP_BASE_URL + "/", {
-        headers: {
-          "Content-type": "application/json",
-        },
-      });
-      setData(data);
-    };
-    setLoading(false);
-  } catch (err) {
-    setError(err);
-    setLoading(false);
-  }
+// const useFetch = () => {
+//   const [data, setData] = useState();
+//   const [loading, setLoading] = useState();
+//   const [error, setError] = useState();
+//   console.log(import.meta.env.VITE_APP_BASE_URL);
+//   try {
+// const fetchData = async () => {
+//   setLoading(true);
+//   const data = await axios.post(import.meta.env.VITE_APP_BASE_URL + "/", {
+//     headers: {
+//       "Content-type": "application/json",
+//     },
+//   });
+//   setData(data);
+// };
+//     setLoading(false);
+//   } catch (err) {
+//     setError(err);
+//     setLoading(false);
+//   }
 
-  return { data, loading, error };
-};
+//   return { data, loading, error };
+// };
 
-export default useFetch;
+// export default useFetch;
